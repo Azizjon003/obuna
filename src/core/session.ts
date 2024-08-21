f73b59b31dca73e06846097dece8b97027d20399
azizjon003 <aliqulovazizjon68@gmail.com>
2024-08-21 22:48:29 +0500

    chore: Update npm dependencies and add telegraf-postgres-session

--- src/core/session.ts
+++ src/core/session.ts
@@ -1,4 +1,11 @@
-import { session as memorySession } from "telegraf";
-const session = memorySession({});
+const PostgresSession = require("telegraf-postgres-session");
+// const session = memorySession({});
+
+const session = new PostgresSession({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export default session;
